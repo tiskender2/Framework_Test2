0c5939d57c91ce7b7bbaf9a101358fcf455072cb
Tolga İskender <tolga@basefy.com>
2020-12-05 18:56:16 +0300

    Initial Commit

---
+++ Framework_Test2/Framework_Test2.h
@@ -0,0 +1,18 @@
+//
+//  Framework_Test2.h
+//  Framework_Test2
+//
+//  Created by Tolga İskender on 5.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Framework_Test2.
+FOUNDATION_EXPORT double Framework_Test2VersionNumber;
+
+//! Project version string for Framework_Test2.
+FOUNDATION_EXPORT const unsigned char Framework_Test2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework_Test2/PublicHeader.h>
+
+
