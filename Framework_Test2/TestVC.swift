cfdfd0aa1a92013aedc50a40151a0948bbe71a10
Tolga İskender <tolga@basefy.com>
2020-12-05 20:45:23 +0300

    first commit

---
+++ Framework_Test2/TestVC.swift
@@ -0,0 +1,29 @@
+//
+//  TestVC.swift
+//  Framework_Test2
+//
+//  Created by Tolga İskender on 5.12.2020.
+//
+
+import UIKit
+
+class TestVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
